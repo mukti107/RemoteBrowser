1e75deb27614ca1fe1e04cd9c403d1e325b94b8e
Rachindra Poudel <mukti107@gmail.com>
2018-09-23 17:24:39 +0545

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const resolve = require('path').resolve
 const app = require('express')()
-const port = 4200
+const port = process.env.NODE_PORT || 4200
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 var io = require('socket.io')(server);
 const browsers = {};
